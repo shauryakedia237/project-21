4194d3dbe769f48fb4e4a8b7b2a7ae04b400e1d3
shauryakedia237 <69040024+shauryakedia237@users.noreply.github.com>
2020-09-25 20:46:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,17 +8,17 @@ function setup() {
   speed=random(223,321);
   thickness=random(22,83);
 
-  bullet=createSprite(50,200,50,50);
+  bullet = createSprite(50,200,50,5);
   bullet.velocityX=speed;
   bullet.shapeColor='white';
 
   wall=createSprite(1200,200,thickness,height/2);
-  wall.shapeColor=color(80,80,80);
+  wall.shapeColor=color(230,230,230);
 
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0);  
 
   if(wall.x-bullet.x < (bullet.width+wall.width)/2){
     bullet.velocityX=0;
@@ -30,6 +30,31 @@ function draw() {
       bullet.shapeColor=color(0,255,0);
     }
   }
-
+  
+   if ( hasCollided (lbullet,lwall)){
+   bullet.velocityX=0; 
+   var damage = 0.5 * weight * speed * speed / ( thickness * thickness * thickness );
+ 
+   if ( damage >= 10){
+     wall.shapeColor=color(255,0,0);
+   } 
+     
+   if ( damage < 10){
+     wall.shapeColor=color(0,255,0);
+   }  
+   
+    
+  }
   drawSprites();
 }
+
+function hasCollided (lbullet,lwall){
+  
+  bulletRightEdge= lbullet.x +lbullet.width;
+  wallLeftEdge= lwall.x;
+  
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+  } 
+    return false;
+}  
